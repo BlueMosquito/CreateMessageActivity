8587eaf534249c3ee7ff1b5bfaa2b42a5319b821
BlueMosquito <61717325+BlueMosquito@users.noreply.github.com>
2021-11-14 23:07:47 +0100

    Latest changes

--- app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
+++ app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
@@ -2,8 +2,10 @@ package com.hfad.mymessenger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class CreateMessageActivity extends AppCompatActivity {
 
@@ -14,6 +16,30 @@ public class CreateMessageActivity extends AppCompatActivity {
     }
 
     public void onSendMessage(View view){
+        EditText messageView = (EditText) findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
 
+        /*
+        Ett Intent med action.
+        Specifierar vilket sort av action dettga är.
+        */
+        Intent intent = new Intent(Intent.ACTION_SEND);
+
+        //Typ av intent är text(String).
+        intent.setType("text/plain");
+
+        /*
+        messageText typ måste vara String i detta fall.
+        Android kollar och tar fram alla aktiviteter som kan skicka ett medelande.
+         */
+        intent.putExtra(Intent.EXTRA_TEXT, messageText);
+
+        /*
+        Här skapas chooser som gör att om det finns många aktiviteter som kan skicka ett medelande
+        så ska alla visas varje gång man vill skicka meddelande.
+         */
+        String chooserTitle = (String) getText(R.string.chooser);
+        Intent chosenIntent = Intent.createChooser(intent, chooserTitle);
+        startActivity(chosenIntent);
     }
 }
