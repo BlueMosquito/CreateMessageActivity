e9ba8c42077c654fd92b03596fc5a073100624e3
Hamee <61717325+BlueMosquito@users.noreply.github.com>
2021-11-15 12:17:24 +0100

    all work

--- app/src/main/java/com/hfad/mymessenger/ReceiveMessageActivity.java
+++ app/src/main/java/com/hfad/mymessenger/ReceiveMessageActivity.java
@@ -8,14 +8,14 @@ import android.widget.TextView;
 
 public class ReceiveMessageActivity extends AppCompatActivity {
 
-    public static final String EXTRA_MESSAGE = "message";
+    public static final String EXTRA_TEXT = "message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive_message);
         Intent intent = getIntent();
-        String messageText = intent.getStringExtra(EXTRA_MESSAGE);
+        String messageText = intent.getStringExtra(Intent.EXTRA_TEXT);
         TextView messageView = (TextView) findViewById(R.id.message);
         messageView.setText(messageText);
     }
